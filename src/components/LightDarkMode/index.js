d478515d518abc906328a58755d1009f1c1fb7c6
Hemanth Kumar <khemanthkumar360@gmail.com>
2022-10-08 20:14:23 +0000

    List Dark Mode-2

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -1,55 +1,32 @@
-// Write your code here
-// Write your code here
 import {Component} from 'react'
 
 import './index.css'
 
 class LightDarkMode extends Component {
   state = {
-    mode: 'Light Mode',
-    text: 'Dark Mode',
-    contentClass: 'light-content',
-    button: 'light-button',
+    isDarkMode: true,
   }
 
-  buttonClick = () => {
-    const {mode} = this.state
-    if (mode === 'Light Mode') {
-      this.setState(() => ({
-        mode: 'Dark Mode',
-        text: 'Light Mode',
-        contentClass: 'dark-content',
-        button: 'dark-button',
-      }))
-      console.log(mode)
-    } else {
-      this.setState(() => ({
-        mode: 'Light Mode',
-        text: 'Dark Mode',
-        contentClass: 'light-content',
-        button: 'light-button',
-      }))
-    }
+  onClickButton = () => {
+    this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
   }
 
   render() {
-    const {text, contentClass, button} = this.state
+    const {isDarkMode} = this.state
+    const modeClassName = isDarkMode ? 'dark-mode' : 'light-mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
     return (
-      <div className="main-container">
-        <div className={`${contentClass} content-container`}>
+      <div className="app-container">
+        <div className={`container ${modeClassName}`}>
           <h1 className="heading">Click To Change Mode</h1>
-          <div>
-            <button
-              className={`${button} btn`}
-              type="button"
-              onClick={this.buttonClick}
-            >
-              {text}
-            </button>
-          </div>
+          <button type="button" onClick={this.onClickButton} className="button">
+            {buttonText}
+          </button>
         </div>
       </div>
     )
   }
 }
+
 export default LightDarkMode
